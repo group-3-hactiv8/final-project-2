64687eaad8a4c211881b5fd7c6241d1ebdf5ab66
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-06-18 21:35:41 +0700

    add validation if email is already used

--- repositories/user_repository/repository.go
+++ repositories/user_repository/repository.go
@@ -7,11 +7,10 @@ import (
 
 type UserRepository interface {
 	GetUserByID(user *models.User) errs.MessageErr
+	GetUserByEmail(user *models.User) errs.MessageErr
 	RegisterUser(user *models.User) (*models.User, errs.MessageErr)
 	LoginUser(user *models.User) errs.MessageErr
 	UpdateUser(user *models.User) (*models.User, errs.MessageErr)
 	DeleteUser(id uint) errs.MessageErr
 	GetUserByIDComment(id uint) (*models.User, errs.MessageErr)
 }
-
-//find one user by id
